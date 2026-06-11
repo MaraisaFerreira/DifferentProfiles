3b4d9ff5c7ddc565adfbf56f04b863f71e9483df
MaraisaFerreira <ferreira.maraisacristina@gmail.com>
2026-06-11 11:39:11 -0300

    init

---
+++ src/main/java/maraisaferreira/com/git/DifferentProfiles/DifferentProfilesApplication.java
@@ -0,0 +1,13 @@
+package maraisaferreira.com.git.DifferentProfiles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DifferentProfilesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DifferentProfilesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/maraisaferreira/com/git/DifferentProfiles/DifferentProfilesApplicationTests.java
@@ -0,0 +1,13 @@
+package maraisaferreira.com.git.DifferentProfiles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DifferentProfilesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
